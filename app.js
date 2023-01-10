1de18995d06c132e5a408dbed1cc00687b520639
Ehplodor <80175881+Ehplodor@users.noreply.github.com>
2023-01-10 11:02:44 +0100

    permission query for camera

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ button.addEventListener('click', showHello);
 
 let video = document.getElementById("webcam");
 
+navigator.permissions.query({name:'camera'}).then(function(permissionStatus) {
+    console.log(permissionStatus.state); // "granted" or "denied"
+});
+
 navigator.mediaDevices.getUserMedia({
   video: { facingMode: "user" },
 }).then(stream => {
