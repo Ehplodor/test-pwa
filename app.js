1d3bc03b085066a09d7a0e71e640bbccb16fd0c6
Ehplodor <80175881+Ehplodor@users.noreply.github.com>
2023-01-10 10:01:30 +0100

    add app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+// check if the browser support service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+        .register('/sw.js')
+        .then((reg) => {
+            console.log('Service worker registered.', reg);
+        })
+        .catch((err) => {
+            console.log('Service worker registration failed:', err);
+        });
+    });
+}
+
+// simple function to show "hello world" message when the button is clicked
+function showHello() {
+    alert('Hello World!');
+}
+
+// add the click event to the button
+const button = document.getElementById('hello-button');
+button.addEventListener('click', showHello);
