d11e12d03eeb0ff2d2068809f76898702ff1a7ba
Ehplodor <80175881+Ehplodor@users.noreply.github.com>
2023-01-12 12:46:04 +0100

    add tracking.min.js from cloudflare and test head tracking

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ function showHello() {
 const button = document.getElementById('hello-button');
 button.addEventListener('click', showHello);
 
-let video = document.getElementById("webcam");
+// let video = document.getElementById("webcam");
+
+const video = document.getElementById('webcam');
+const canvas = document.createElement('canvas');
+
 
 navigator.mediaDevices.getUserMedia({
   video: { facingMode: "user" },
@@ -19,4 +23,21 @@ navigator.mediaDevices.getUserMedia({
   console.log("Error: " + error);
 });
 
+const tracker = new tracking.ObjectTracker('face');
+tracker.setInitialScale(4);
+tracker.setStepSize(2);
+tracker.setEdgesDensity(0.1);
 
+tracking.track('#webcam', tracker, { camera: true });
+
+tracker.on('track', function(event) {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  event.data.forEach(function(rect) {
+      context.strokeStyle = '#a64ceb';
+      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      context.font = '11px Helvetica';
+      context.fillStyle = "#fff";
+      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+  });
+});
