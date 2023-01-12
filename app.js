b2f74d5dcf8f650df793a2c8af1f1ddc46985492
Ehplodor <80175881+Ehplodor@users.noreply.github.com>
2023-01-12 11:17:59 +0100

    Update app.js
    delete registration function (put in separate js file included in head of index.html

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// check if the browser support service worker
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-        .register('./service-worker.js')
-        .then((reg) => {
-            console.log('Service worker registered.', reg);
-        })
-        .catch((err) => {
-            console.log('Service worker registration failed:', err);
-        });
-    });
-}
-
-
 // simple function to show "hello world" message when the button is clicked
 function showHello() {
     alert('Hello World!');
